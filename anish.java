734d5f1cc2eed5296cefa3ed48f0623f5ebf0b27
anishraza23 <anishraza23@gmaul.com>
2025-09-29 12:04:47 +0530

    Add files via upload

---
+++ anish.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class anish {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("ek number do: ");
+    int a = sc.nextInt();
+    for (int i = 1; i <= 10; i++) {
+        System.out.println( a + "x" + i + "=" +a * i);
+        
+        
+    }System.out.println("l");
+}
+}
